7ae95367254d133576188080e6e7a0a400f01f9e
RandomKiddo <nghugare2@outlook.com>
2021-07-02 21:47:49 -0400

    LibGDX Initial Commit

---
+++ desktop/src/me/firsttry/dungeons/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.firsttry.dungeons.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.firsttry.dungeons.Dungeons;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Dungeons(), config);
+	}
+}
